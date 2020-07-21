f4c1faf28482de9d2c18373f4653177958c70b7b
Sunny <thstkd92@gmail.com>
2020-07-21 12:13:42 +0900

    다이나믹 컴포턴트 사용하기

--- movie-app-2020/src/App.js
+++ movie-app-2020/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import Tomato from "./Tomato";
+
+function Food({ name, picture }) { 
+  return <div>
+    <h2>사실 { name } 조아함.</h2>
+    <img src={ picture } />
+  </div>
+}
+
+const foodILike = [
+  {
+    name: "도넛",
+    image: 
+    "https://imboldn.com/wp-content/uploads/2018/04/Krispy-Kreme-The-Simpsons-Dohnut-main.jpg"
+  },
+  {
+    name: "피자",
+    image: "https://akamai.pizzahut.co.kr/images/products/list/P_P_SS.jpg"
+  },
+  {
+    name: "햄버거",
+    image: "https://image.chosun.com/sitedata/image/202001/13/2020011303147_0.jpg"
+
+  },
+];
 
 function App() {
   return (
     <div className="App"> 
-      <h1>hello!!!</h1>
-      <Tomato />
+      {foodILike.map(dish => (
+      <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
--- movie-app-2020/src/Tomato.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Tomato() {
-    return <h2>I hate Tomato</h2>
-}
-
-export default Tomato;
