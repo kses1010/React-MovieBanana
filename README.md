11188e50e5ef2a7d74be065f3c09ccafe1a20cfe
Sunny <thstkd92@gmail.com>
2020-07-21 12:47:13 +0900

    prop-types 적용하기

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # React-MovieBanana
 영화 평론 사이트 클론 코딩
+
+## Movie App 2020
+
+React JS Fundamentals Course (2020 Start!)
--- movie-app-2020/src/App.js
+++ movie-app-2020/src/App.js
@@ -1,34 +1,48 @@
 import React from 'react';
-
-function Food({ name, picture }) { 
-  return <div>
-    <h2>사실 { name } 조아함.</h2>
-    <img src={ picture } />
-  </div>
-}
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
+    id: 1,
     name: "도넛",
     image: 
-    "https://imboldn.com/wp-content/uploads/2018/04/Krispy-Kreme-The-Simpsons-Dohnut-main.jpg"
+    "https://imboldn.com/wp-content/uploads/2018/04/Krispy-Kreme-The-Simpsons-Dohnut-main.jpg",
+    rating: 5
   },
   {
+    id: 2,
     name: "피자",
-    image: "https://akamai.pizzahut.co.kr/images/products/list/P_P_SS.jpg"
+    image: "https://akamai.pizzahut.co.kr/images/products/list/P_P_SS.jpg",
+    rating: 3
   },
   {
+    id: 3,
     name: "햄버거",
-    image: "https://image.chosun.com/sitedata/image/202001/13/2020011303147_0.jpg"
+    image: "https://image.chosun.com/sitedata/image/202001/13/2020011303147_0.jpg",
+    rating: 2.5
 
   },
 ];
 
+function Food({ name, picture, rating }) { 
+  return <div>
+    <h2>사실 { name } 조아함.</h2>
+    <h4>{rating}/5.0</h4>
+    <img src={ picture } alt={name}/>
+  </div>
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+
 function App() {
   return (
     <div className="App"> 
-      {foodILike.map(dish => (
-      <Food name={dish.name} picture={dish.image} />
+      {foodILike.map((dish, index) => (
+      <Food key={index} name={dish.name} picture={dish.image} rating={dish.rating} />
       ))}
     </div>
   );
