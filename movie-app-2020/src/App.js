3e2b23b014b859add0541977d7490f54d052fdd8
Sunny <thstkd92@gmail.com>
2020-07-23 22:08:19 +0900

    props 공유 연습

--- movie-app-2020/src/App.js
+++ movie-app-2020/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import {HashRouter, Route} from "react-router-dom";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
-  return <HashRouter>
+  return ( 
+  <HashRouter>
+    <Navigation />
     <Route path="/" exact={true} component={Home} />
     <Route path="/about" component={About}/>
-  </HashRouter> 
+    <Route path="/movie-detail" component={Detail}/>
+
+  </HashRouter>
+  ); 
 }
 
 export default App;
--- movie-app-2020/src/components/Movie.js
+++ movie-app-2020/src/components/Movie.js
@@ -1,9 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
+    <Link to={{
+      pathname: "/movie-detail",
+      state: {
+        year,
+        title,
+        summary,
+        poster,
+        genres
+      }
+    }}>
   <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie_data">
@@ -19,7 +30,8 @@ function Movie({ year, title, summary, poster, genres }) {
         <p className="movie_summary">{summary.slice(0, 180)}...</p>
       </div>
   </div>
-  )
+  </Link>
+  );
 };
 
 Movie.propTypes = {
